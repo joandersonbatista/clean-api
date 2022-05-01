5c5259bb948b1d5a3669e1ea71f495b6924c8f5f
Joanderson <mr_prince@pop-os.localdomain>
2022-05-01 10:27:59 -0300

    chore: refactor setup

--- src/presentation/errors/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './MissingParam.error';
-export * from './InvalidParam.error';
-export * from './Server.error';
--- src/presentation/protocols/Controller.protocol.ts
+++
@@ -1,5 +0,0 @@
-import { HttpResponse, HttpRequest } from './Http.protocol';
-
-export interface Controller {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>;
-}
